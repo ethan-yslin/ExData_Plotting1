aa637ac5da99ba1665ca8206158aee01eff6359b
ethan-yslin <ethanlin0822@gmail.com>
2021-09-07 14:27:25 -0400

    update

---
+++ plot2.R
@@ -0,0 +1,17 @@
+# This is to generate plot2.png.
+
+# Load libraries.
+library(data.table)
+library(lubridate)
+
+# Read and prepare data.
+dt_raw <- fread("unzip -p exdata_data_household_power_consumption.zip")
+dt <- dt_raw %>% 
+  filter(Date %in% c("1/2/2007","2/2/2007")) %>%
+  mutate(Date_Time = dmy_hms(paste(Date, Time)))
+dt[,(3:8) := lapply(.SD, as.numeric), .SDcols = 3:8] # convert cols to numeric
+
+# Plot.
+png("plot2.png", width = 480, height = 480)
+
+dev.off()
